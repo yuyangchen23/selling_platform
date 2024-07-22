95499cd5b45efb5e60a500c866b030304e71d750
yuyangchen23 <bryanchen0923@gmail.com>
2024-07-21 17:22:55 -0700

    created a database for storing clients' information

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
